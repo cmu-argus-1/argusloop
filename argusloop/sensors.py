7e7e97597a658e8e40ae5127cd8b309d36e52356
ibrahima <ibrahima.sr.sow@gmail.com>
2024-07-01 01:59:00 +0200

    added in_eclipse function

--- argusloop/sensors.py
+++ argusloop/sensors.py
@@ -109,6 +109,38 @@ class SunVector:
         return nu
 
 
+
+
+def in_eclipse(sun_vector, x_pos_eci):
+    """
+    Computes the illumination fraction of a satellite in Earth orbit using a
+    cylindrical Earth shadow model.
+
+    Parameters:
+        sun_vector (np.array): Sun vector in inertial frame.
+        x_pos_eci (np.array): Position of the satellite in the ECI frame.
+
+    Returns:
+        illumination_param (float): Illumination fraction (0 <= nu <= 1). nu = 0 means spacecraft in complete shadow,
+        nu = 1 mean spacecraft fully illuminated by sun.
+
+    References:
+    1. O. Montenbruck, and E. Gill, Satellite Orbits: Models, Methods
+                                    and Applications_, 2012, p.80-83.
+    """
+    # Projection of spacecraft position
+    s = np.dot(x_pos_eci, sun_vector)
+
+    # Compute illumination parameter
+    illumination_param = True
+    if (
+        s / np.linalg.norm(s) >= 1.0
+        or np.linalg.norm(x_pos_eci - s * sun_vector) > 6378.0e3
+    ):
+        illumination_param = False
+
+    return illumination_param
+
 class GPS:
     def __init__(self, position_noise_std, velocity_noise_std):
         self.pos_std = position_noise_std
